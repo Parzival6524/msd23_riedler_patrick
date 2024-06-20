3d296a66280b6b02b5511bbfafb3aaca16db1d8a
Patrick Riedler <patrick.riedler@edu.fh-joanneum.at>
2024-06-20 07:47:04 +0200

    ''

---
+++ Uebung_5/exercise5.md
@@ -0,0 +1,8 @@
+Übung 5:
+
+Auffälligkeiten:
+
+Der Output der Konsole beinhaltet nur den logger.errror und nicht de logger.info text.
+
+![Image](resources/images/ex5_2.jpg)
+
--- src/main/java/at/fhj/msd/main.java
+++ src/main/java/at/fhj/msd/main.java
@@ -1,12 +1,21 @@
 package at.fhj.msd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class main {
+
+    private static Logger logger = LogManager.getLogger();
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        System.out.println(calculator.add(5,3.5));
-        System.out.println(calculator.minus(5,3.5));
-        System.out.println(calculator.divide(5,3.5));
-        System.out.println(calculator.multiply(5,3.5));
+        System.out.println(calculator.add(5, 3.5));
+        System.out.println(calculator.minus(5, 3.5));
+        System.out.println(calculator.divide(5, 3.5));
+        System.out.println(calculator.multiply(5, 3.5));
         System.out.println("Patrick Riedler");
+
+        logger.info("INFO");
+        logger.error("ERROR");
     }
 }
